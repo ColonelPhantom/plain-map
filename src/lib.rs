5e58e7f2bb16470307d1c1725a0ac776f5e8c926
Quinten Kock <quintenkock@gmail.com>
2019-09-29 01:07:19 +0200

    Implement most functions from std::HashMap

--- src/lib.rs
+++ src/lib.rs
@@ -2,7 +2,7 @@
 // mod tests {
 //     #[test]
 //     fn it_works() {
-//         assert_eq!(2 + 2, 4);
+//         let m = crate::PlainMap::new();
 //     }
 // }
 
@@ -18,6 +18,20 @@ impl<Key: PartialEq, Value> PlainMap<Key, Value> {
 
         }
     }
+    pub fn with_capacity(capacity: usize) -> Self {
+        Self {
+            pairs: Vec::with_capacity(capacity),
+        }
+    }
+
+    pub fn reserve(&mut self, additional: usize) {
+        self.pairs.reserve(additional)
+    }
+    pub fn shrink_to_fit(&mut self) {
+        self.pairs.shrink_to_fit();
+    }
+
+
     fn find(&self, k: &Key) -> Option<usize> {
         self.pairs.iter().position(|(x,_)| *x == *k)
     }
@@ -28,32 +42,105 @@ impl<Key: PartialEq, Value> PlainMap<Key, Value> {
             None => self.pairs.push((k,v)),
         }
     }
+    pub fn insert(&mut self, k: Key, v: Value) -> Option<Value> {
+        match self.find(&k) {
+            Some(i) => {
+                let ov = self.pairs.remove(i).1;
+                self.pairs.push((k,v));
+                Some(ov)
+            },
+            None => {
+                self.pairs.push((k,v));
+                None
+            }
+        }
+    }
+
     pub fn get(&self, k: &Key) -> Option<&Value> {
         match self.find(k) {
             Some(i) => Some(&self.pairs[i].1),
             None => None
         }
     }
-    pub fn delete(&mut self, k: &Key) {
-        match self.find(&k) {
-            Some(i) => {
-                self.pairs.remove(i);
-            },
-            None => {}
+    pub fn get_mut(&mut self, k: &Key) -> Option<&mut Value> {
+        match self.find(k) {
+            Some(i) => Some(&mut self.pairs[i].1),
+            None => None
+        }
+    }
+    pub fn get_key_value(&self, k: &Key) -> Option<&(Key, Value)> {
+        match self.find(k) {
+            Some(i) => Some(&self.pairs[i]),
+            None => None
+        }
+    }
+    pub fn get_key_value_mut(&mut self, k: &Key) -> Option<&mut (Key, Value)> {
+        match self.find(k) {
+            Some(i) => Some(&mut self.pairs[i]),
+            None => None
         }
     }
 
-    pub fn len(&self) -> usize {
-        self.pairs.len()
+    pub fn contains_key(&self, k: &Key) -> bool {
+        self.find(k).is_some()
     }
-    pub fn iter(&self) -> std::slice::Iter<(Key, Value)> {
-        self.pairs.iter()
+
+    pub fn remove(&mut self, k: &Key) -> Option<Value> {
+        match self.find(k) {
+            Some(i) => Some(self.pairs.remove(i).1),
+            None => None,
+        }
     }
+    pub fn remove_entry(&mut self, k: &Key) -> Option<(Key, Value)> {
+        match self.find(k) {
+            Some(i) => Some(self.pairs.remove(i)),
+            None => None,
+        }
+    }
+
     pub fn entry(&mut self, k: Key) -> PlainMapEntry<Key, Value> {
         PlainMapEntry {
             parent: self,
             k: k
         }
+    }
+
+    pub fn capacity(&self) -> usize {
+        self.pairs.capacity()
+    }
+    pub fn len(&self) -> usize {
+        self.pairs.len()
+    }
+    pub fn is_empty(&self) -> bool {
+        self.pairs.is_empty()
+    }
+
+    pub fn clear(&mut self) {
+        self.pairs.clear();
+    }
+
+    pub fn iter(&self) -> std::slice::Iter<(Key, Value)> {
+        self.pairs.iter()
+    }
+    pub fn iter_mut(&mut self) -> impl Iterator<Item = &mut (Key, Value)> {
+        self.pairs.iter_mut()
+    }
+
+    pub fn keys(&self) -> impl Iterator<Item = &Key> {
+        self.pairs.iter().map(|(k,_v)| k)
+    }
+    pub fn keys_mut(&mut self) -> impl Iterator<Item = &mut Key> {
+        self.pairs.iter_mut().map(|(k,_v)| k)
+    }
+    pub fn values(&self) -> impl Iterator<Item = &Value> {
+        self.pairs.iter().map(|(_k,v)| v)
+    }
+    pub fn values_mut(&mut self) -> impl Iterator<Item = &mut Value> {
+        self.pairs.iter_mut().map(|(_k,v)| v)
+    }
+
+    pub fn drain(&mut self) -> std::vec::Drain<'_, (Key, Value)> {
+        self.pairs.drain(..)
     }
 }
 
